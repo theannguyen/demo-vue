6569a33901013d891d8095b2a778f3592533525d
theannguyen <nguyenthean17091996@gmail.com>
2023-09-08 17:29:53 +0700

    update

---
+++ src/components/Alert.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <div v-if="openClose" class="modal fade show" tabindex="-1" aria-labelledby="exampleModalLabel" aria-modal="true"
+            role="dialog" style="display: block">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Demo Modal</h5>
+                        <button @click="openCloseFunc()" type="button" class="btn-close" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <input>
+                    </div>
+                    <div class="modal-footer">
+                        <button @click="openCloseFunc()" type="button" :class="'btn btn-' + variant">Close</button>
+                        <button @click="openCloseFunc()" type="button" :class="'btn btn-primary'">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>aaavdffsdfsd
+        <button @click="openCloseFunc()" type="button" :class="'btn btn-success'">Open Modal</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Alert',
+    props: {
+        visible: Boolean,
+        variant: String,
+    },
+    data() {
+        return {
+            openClose: this.visible
+        }
+    },
+    methods: {
+        openCloseFunc() {
+            this.openClose = !this.openClose;
+        }
+    },
+    watch: {
+        visible: function (newValue, oldValue) {
+            this.openClose = newValue
+            console.log('new' + newValue + '==' + oldValue)
+        }
+    }
+}
+</script>
---
+++ src/components/Index.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Alert from './Alert.vue'
+</script>
+
+<template>
+    <Alert visible="false" variant="secondary" />
+</template>
+
+<style scoped></style>
